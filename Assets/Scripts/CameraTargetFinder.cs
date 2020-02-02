74216283867e6b38b3764cfa77d44f5ab97d53dd
Seppi <schuhmannsebastian@gmail.com>
2020-02-02 13:20:57 +0100

    Added ML-Agents 0.13.1, built first prototype of Scene

---
+++ Assets/Scripts/CameraTargetFinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CameraMultiTarget))]
+public class CameraTargetFinder : MonoBehaviour
+{
+    private CameraMultiTarget multiTargetCam;
+
+    public GameObject[] targets;
+
+    void Start()
+    {
+        multiTargetCam = GetComponent<CameraMultiTarget>();
+        multiTargetCam.SetTargets(targets);
+    }
+
+    void Update()
+    {
+        
+    }
+}
